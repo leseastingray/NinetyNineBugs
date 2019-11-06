75c02b7808cfed9d40bace9efd7f2c1fb7272ae0
leseastingray <lesea@my.lanecc.edu>
2019-11-06 15:47:43 -0800

    two layouts, most widgets placed, able to move between activities

--- app/src/main/java/com/alese/ninetyninebugs/MainActivity.java
+++ app/src/main/java/com/alese/ninetyninebugs/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     // Widget variables
+    TextView bugNumberTextView;
     Button takeOneButton;
 
     // Other variables
     Integer bugNumber = 99;
+    public static final String BUGS = "bugs";
+    public static final int REQUEST_1 = 1;
 
     /* ------------- Activity Callback Methods --------------*/
     @Override
@@ -22,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Set up widget variables
+        bugNumberTextView = (TextView)findViewById(R.id.bugNumberTextView);
         takeOneButton = (Button)findViewById(R.id.takeOneButton);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // manipulate bugNumberTextView
+        bugNumberTextView.setText(data.getStringExtra(BUGS) + " Bugs on the Wall");
     }
 
     /* ------------- Event Handlers --------------*/
@@ -31,5 +44,10 @@ public class MainActivity extends AppCompatActivity {
     public void takeOne(View v)
     {
         Toast.makeText(this, Integer.toString(bugNumber), Toast.LENGTH_SHORT).show();
+        // Start second activity and send info from main activity
+        Intent intent = new Intent(this, SecondActivity.class);
+        // Sends data to 2nd activity, turns bugNumber to a string
+        intent.putExtra(BUGS, Integer.toString(bugNumber));
+        startActivityForResult(intent, REQUEST_1);
     }
 }
--- app/src/main/java/com/alese/ninetyninebugs/SecondActivity.java
+++ app/src/main/java/com/alese/ninetyninebugs/SecondActivity.java
@@ -1,4 +1,44 @@
 package com.alese.ninetyninebugs;
 
-public class SecondActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    /* ------------- Activity Callback Methods --------------*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // Get intent from MainActivity
+        Intent intent = getIntent();
+
+    }
+
+    /* ------------- Event Handlers --------------*/
+    // Method for patching bug (subtracting it from total)
+    public void patchBug()
+    {
+
+    }
+    public void bugOut(View v)
+    {
+        Intent bugs = new Intent(this, MainActivity.class);
+        //bugs.putExtra(MainActivity.BUGS, Integer.toString(bugNumber));
+        //setResult(REQUEST_OK, bugs);
+        finish();
+    }
 }
